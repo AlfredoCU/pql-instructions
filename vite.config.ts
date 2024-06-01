f67f244127f7b9e3b21a564ec0196d5dd8462751
AlfredoCU <alfredo_11_cu@hotmail.com>
2024-06-01 15:30:34 -0600

    chore: svgr config created

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 // @ts-ignore
 import path from "path";
 import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 import react from "@vitejs/plugin-react";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // @ts-ignore
   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
-  plugins: [react()]
+  plugins: [react(), svgr()]
 });
