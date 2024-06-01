21f00283ee2ea8b370bfdd6182496d28885d4613
AlfredoCU <alfredo_11_cu@hotmail.com>
2024-06-01 14:41:27 -0600

    chore: alias config created

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,11 @@
+// @ts-ignore
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({ plugins: [react()] });
+export default defineConfig({
+  // @ts-ignore
+  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
+  plugins: [react()]
+});
